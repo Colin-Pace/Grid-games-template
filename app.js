688dd2efc54fa94b4b01a4ca455add6b144e23db
Colin Pace <colinpace0001@gmail.com>
2020-09-23 11:11:44 -0500

    index 15

--- app.js
+++ app.js
@@ -12,19 +12,19 @@ document.addEventListener('DOMContentLoaded', () => {
   function movePlayer(e) {
     grid[index].classList.remove('player');
     switch(e.keyCode) {
-      case 37:
+      case 37: // l
         if (index % limit === 0) index += 14;
         else if (index % limit !== 0) index -= 1;
         break;
-      case 38:
+      case 38: // u
         if (index <= 14 && index >= 0) index+= 210;
-        else if (index > 15) index -= 15;
+        else if (index > 14) index -= 15;
         break;
-      case 39:
+      case 39: // r
         if (index % limit >= limit - 1) index -= 14;
         else if (index % limit < limit - 1) index += 1;
         break;
-      case 40:
+      case 40: // d
         if (index >= 210) index -= 210;
         else if (index < 210) index += 15;
         break;
